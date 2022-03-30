e0a8f3e9ac1b89e9c7109aa6ac877f7fa2248cea
JoeLuciano <joeluciano@outlook.com>
2022-03-30 00:12:17 -0400

    Failed miserably

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import { motion } from 'framer-motion';
+import { Melon } from './components/symbolsSVGs/Melon';
 import './App.css';
 
+const SvgPage = (props) => {
+  const svgPage = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.5 },
+    },
+  };
+  return (
+    <motion.div variants={svgPage} initial='hidden' animate='visible'>
+      {props.children}
+    </motion.div>
+  );
+};
+
+const SvgRow = (props) => {
+  const svgRow = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.5 },
+    },
+  };
+  return <motion.div variants={svgRow}>{props.children}</motion.div>;
+};
+
 function App() {
-  return <div>test</div>;
+  return (
+    <div className='app'>
+      <SvgPage>
+        <SvgRow>
+          <Melon />
+          <Melon />
+          <Melon />
+          <Melon />
+        </SvgRow>
+        <SvgRow>
+          <Melon />
+          <Melon />
+          <Melon />
+          <Melon />
+        </SvgRow>
+      </SvgPage>
+    </div>
+  );
 }
 
 export default App;
